aacd144eac81bf5835e0d62f5896b904ad26b752
sennevb <sennevanbaelen@gmail.com>
2017-05-21 17:12:26 +0200

    Update ZcashStratum.cpp

--- nheqminer/libstratum/ZcashStratum.cpp
+++ nheqminer/libstratum/ZcashStratum.cpp
@@ -527,7 +527,7 @@ ZcashJob* ZcashMiner<CPUSolver, CUDASolver, OPENCLSolver>::parseJob(const Array&
     // TODO: On a LE host shouldn't this be le32toh?
     ret->header.nVersion = be32toh(version);
 
-    //if (ret->header.nVersion == 4) {
+    if (ret->header.nVersion > 29) {
         if (params.size() < 8) {
             throw std::logic_error("Invalid job params");
         }
@@ -553,9 +553,9 @@ ZcashJob* ZcashMiner<CPUSolver, CUDASolver, OPENCLSolver>::parseJob(const Array&
 
         ret->time = params[5].get_str();
         ret->clean = params[7].get_bool();
-    /*} else {
+    } else {
         throw std::logic_error("ZcashMiner::parseJob(): Invalid or unsupported block header version");
-    }*/
+    }
 
     ret->header.nNonce = nonce1;
     ret->nonce1Size = nonce1Size;
